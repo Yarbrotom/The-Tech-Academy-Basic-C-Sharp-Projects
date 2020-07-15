49b407642020c9536f565e5145c0d37fd5bfcbdc
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-15 12:00:57 -0700

    Add report assignment
    C# & .NET FRAMEWORK 2020 step: 70

---
+++ VS Assignment/myConsoleProject/AcmeAccounting/reportAssignment/reportAssignment/reportAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reportAssignment
+{
+    class Program
+    {
+        // Report program
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string page = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string help = Console.ReadLine();
+            Console.WriteLine("Where there any positive experiences you'd like to share? Please give specifics.");
+            string experience = Console.ReadLine();
+            Console.WriteLine("Is there any feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today? Please enter a number");
+            string hours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
