161dda4fe0d3a267ec13bb39c5e775d5b84f1916
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-15 12:42:51 -0700

    Add Math Assignment
    C# and .NET FRAMEWORK 2020 step: 87

---
+++ MathAndComparisonOperators/MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine("Five plus Ten = " + total.ToString());
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //double quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemp = 70;
+            //int currentTemp = 72;
+
+            //bool isWarm = currentTemp > roomTemp;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            Console.WriteLine("Please enter a whole number between 1 and 10,000,00.");
+            string input = Console.ReadLine();
+            double number = Int32.Parse(input);
+            Console.WriteLine(number * 50);
+            Console.WriteLine(number + 25);
+            Console.WriteLine(number / 12.5);
+            Console.WriteLine(number > 50);
+            Console.WriteLine(number % 7);
+            Console.ReadLine();
+            
+        }
+    }
+}
