2e383872ff4233d5204d720059046a6444ff9c3b
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-16 10:55:48 -0700

    Add Boolean Logic Assignment
    C# & .NET FRAMEWORK 2020 step 99

---
+++ Boolean/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (please type, true or false)");
+            bool dui = bool.Parse(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && dui == false && tickets <= 3);
+            Console.ReadLine();
+        }
+    }
+}
