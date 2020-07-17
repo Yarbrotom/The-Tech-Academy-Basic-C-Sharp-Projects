8ca1f7b5f95686affac6ebc5bdcaed51b1064644
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-17 10:07:38 -0700

    Add String Functions Assignment
    C# & .NET FRAMEWORK 2020 step: 124

---
+++ StringFunctions/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = "Mount";
+            string secondWord = "Hood";
+            string thirdWord = "Meadows";
+
+            string name = firstWord + " " + secondWord + " " + thirdWord;
+
+            Console.WriteLine(name.ToUpper());
+
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("The Meadows Difference - ");
+            sb.Append("Our Awesome Terrain");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
