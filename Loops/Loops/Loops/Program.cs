58bcfa4c66faaf8002ea9f07c8ed21a7b8d894a5
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-17 09:45:00 -0700

    Add Loop Assignment
    C# & .NET FRAMEWORK 2020 step: 120

---
+++ Loops/Loops/Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endLoop = false;
+            while (endLoop)
+            {
+                Console.WriteLine("This loop is ending.");
+                Console.ReadLine();
+            }
+
+            bool endDoWhileLoop = true;
+
+            do
+            {
+                Console.WriteLine(" The do while loop is ending.");
+                Console.ReadLine();
+            }
+            while (endDoWhileLoop);
+        }
+    }
+}
