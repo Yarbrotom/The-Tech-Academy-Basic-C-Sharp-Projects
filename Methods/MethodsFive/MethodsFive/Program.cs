fb3d5c7181d4b35c1f9f9768d1de43886c796565
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-19 11:32:30 -0700

    Fifth Method Assignment
    C# & .NET FRAMEWORK 2020 step: 169

---
+++ Methods/MethodsFive/MethodsFive/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to divide by two:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int answer;
+            divide.Division(x, out answer);
+            Console.WriteLine("The answer is: " + answer.ToString());
+
+            Console.WriteLine("Please enter another number that will be multiplied by 2:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter one more number that will be multiplied by 3:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int answerOne;
+            int answerTwo;
+            divide.Division(x, y, out answerOne, out answerTwo);
+            Console.WriteLine("The first answer is: " + answerOne.ToString());
+            Console.WriteLine("The second answer is: " + answerTwo.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods/MethodsFive/MethodsFive/divide.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsFive
+{
+    public static class divide
+    {
+        public static void Division(int x, out int answer)
+        {
+            answer = Convert.ToInt32(x / 2);
+        }
+
+        public static void Division(int x, int y, out int answerOne, out int answerTwo)
+        {
+            answerOne = x * 2;
+            answerTwo = y * 3;
+        }
+    }
+}
