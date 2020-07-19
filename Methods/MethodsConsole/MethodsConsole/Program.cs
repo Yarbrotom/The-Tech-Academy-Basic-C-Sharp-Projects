47e8c87694f888f0e7cc387b80ab0818e9d150fb
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-19 09:31:27 -0700

    Add Methods Assignment
    C# & .NET FRAMEWORK 2020 step: 161

---
+++ Methods/MethodsConsole/MethodsConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number for the program to do \"Quick Maths\" to:");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The first opperation returns " + QuickMaths.AddFive(answer).ToString());
+            Console.ReadLine();
+
+            Console.WriteLine("The second opperation returns " + QuickMaths.SubFive(answer).ToString());
+            Console.ReadLine();
+
+            Console.WriteLine("The third opperation returns " + QuickMaths.AddTen(answer).ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods/MethodsConsole/MethodsConsole/QuickMaths.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsConsole
+{
+    public class QuickMaths
+    {
+        static public int AddFive(int x)
+        {
+            return x + 5;
+        }
+
+        static public int SubFive(int x)
+        {
+            return x - 5;
+        }
+
+        static public int AddTen(int x)
+        {
+            return x + 10;
+        }
+    }
+}
