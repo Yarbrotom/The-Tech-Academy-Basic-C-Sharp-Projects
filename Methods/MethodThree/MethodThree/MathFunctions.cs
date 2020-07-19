2a14ec14018afab23642a81bc797269627221f45
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-19 10:48:55 -0700

    Add Third Methods Asssignment
    C# & .NET FRAMEWORK 2020 step: 165

---
+++ Methods/MethodThree/MethodThree/MathFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodThree
+{
+    public class MathFunctions
+    {
+        public static int AddTwo(int x, int y = 1)
+        {
+            return x + y;
+        }
+
+    }
+}
---
+++ Methods/MethodThree/MethodThree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number:");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Optional: Please enter another whole number:");
+
+            string second = Console.ReadLine();
+
+
+            if(second != "")
+            {
+                Console.WriteLine(MathFunctions.AddTwo(first, Convert.ToInt32(second)).ToString());
+            }
+            else
+            {
+                Console.WriteLine(MathFunctions.AddTwo(first).ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
