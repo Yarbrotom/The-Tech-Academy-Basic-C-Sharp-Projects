89c4eedf035cd2cc88c47a1adae252cb85c49293
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-26 16:23:37 -0700

    Add Enums Assignment
    C# & .NET FRAMEWORK 2020 step: 195

---
+++ Enums/Enums/Enums/Days.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Days
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+    }
+
+    public enum DayOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ Enums/Enums/Enums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days days = new Days();
+            
+            Console.WriteLine("Please enter the current day of the week: ");
+            try
+            {
+                
+                string userDay = Console.ReadLine();
+                DayOfWeek dayOfWeek = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), userDay);
+                
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
