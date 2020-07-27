c8ad6e0080026986b4c883c60b2cce5e8f029872
Thomas Yarbrough <yarbrotom@gmail.com>
2020-07-26 17:10:17 -0700

    Add Struct Assignment
    C# & .NET FRAMEWORK 2020 step: 200

---
+++ Struct/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 9.99m;
+
+            Console.WriteLine(number.Amount.ToString());
+            Console.ReadLine();
+        }
+    }
+}
