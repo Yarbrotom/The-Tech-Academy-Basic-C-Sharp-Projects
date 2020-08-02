d0e6403505218ff5f52784f18bdcb4b08678a9cf
Thomas Yarbrough <yarbrotom@gmail.com>
2020-08-02 10:30:30 -0700

    Add DateTime Assignment
    C# & .NET FRAMEWORK 2020 step: 222

---
+++ DateTime Assignment/DateTime Assignment/DateTime Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine("Please enter a whole number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            DateTime futureTime = now.AddHours(number);
+            Console.WriteLine("In " + number + " hours the time will be " + futureTime);
+            Console.ReadLine();
+
+        }
+    }
+}
